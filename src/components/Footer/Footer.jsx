1c8c1deedf6344b275c4ffae471c8384b21fa70d
nipunac99 <nipunachamod99723@gmail.com>
2024-09-02 16:35:48 +0530

    finish_navbar_and_footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Woman</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur commodi numquam doloremque! Totam nihil, laborum minima
+            error veritatis facilis fuga explicabo blanditiis. Odit, saepe.
+            Ipsum.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact Us</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            tempora nam praesentium libero debitis magni sed reiciendis
+            architecto expedita nisi nulla modi, ex non distinctio.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">NC STORE</span>
+          <span className="copyright">© Copyright 2024. All Rights Reserved</span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
